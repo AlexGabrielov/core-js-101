c7e89fbb8647b5bead5a678151e023ae43727803
Alex <alex.gabriel@mail.ru>
2020-05-12 21:23:55 +0300

    feat: solve easy 04 tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 
@@ -73,8 +74,22 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let timeInMs = endDate - startDate;
+  let hours = Math.floor(timeInMs / (60 * 60 * 1000));
+  timeInMs -= hours * (1000 * 60 * 60);
+  let minutes = Math.floor((timeInMs / (60 * 1000)));
+  timeInMs -= minutes * (1000 * 60);
+  let seconds = Math.floor((timeInMs / 1000));
+  timeInMs -= seconds * 1000;
+  const zero = '0';
+  if (hours <= 9) hours = zero + hours;
+  if (minutes <= 9) minutes = zero + minutes;
+  if (seconds <= 9) seconds = zero + seconds;
+  if (timeInMs <= 9) timeInMs = zero + zero + timeInMs;
+
+
+  return `${hours}:${minutes}:${seconds}.${timeInMs}`;
 }
 
 
