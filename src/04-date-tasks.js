1e20df77f9cc020ce627cebadfe7b3469b979ebf
Alex <alex.gabriel@mail.ru>
2020-05-16 21:01:10 +0300

    feat: solve all 05 tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -109,8 +109,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getHours() % 12;
+  const minutes = date.getMinutes();
+  const res = 0.5 * (60 * hours * minutes) - 6 * minutes;
+  return res;
 }
 
 
